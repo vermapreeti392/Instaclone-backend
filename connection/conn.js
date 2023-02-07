6561653275542fab420e339aa203de93cc746aeb
vermapreeti392 <vermapreeti392@gmail.com>
2023-02-07 21:33:26 +0530

    backend

--- connection/conn.js
+++ connection/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const {keys} = require('../keys')
-
+mongoose.set(`strictQuery`, true)
 
 async function getConnection (){
    await mongoose.connect(keys).then(()=>{
